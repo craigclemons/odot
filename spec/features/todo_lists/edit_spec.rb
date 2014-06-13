eae2ce279db2750a9f0899f5164da4d784c16bc1
Craig Clemons <engrclemons@gmail.com>
2014-06-13 10:34:17 -0400

    Edits made at Wednesday meeting. Added a = to one of my views. app/views/todo_lists/index.html.erb and in addition removed an unneeded ! on my /spec/features/todo_lists/edit_spec.rb file

--- spec/features/todo_lists/edit_spec.rb
+++ spec/features/todo_lists/edit_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Editing todo lists" do
- let!~(:todo_list) {TodoList.create(title: "Groceries", description: "Grocery List") }
+ let!(:todo_list) {TodoList.create(title: "Groceries", description: "Grocery List") }
  
 
   def update_todo_list(options={})
@@ -9,7 +9,7 @@ describe "Editing todo lists" do
    options[:description] ||= "This is my todo list."
 
    todo_list = options[:todo_list]   
-   visit "/todo_list"
+   visit "/todo_lists"
    within "#todo_list_#{todo_list.id}" do
 	click_link "Edit"
       end
@@ -18,13 +18,30 @@ describe "Editing todo lists" do
 	click_button "Update Todo_list"
    end
   it "updates a todo list succesfully with the correct information" do
-	update_todo_list todo_list: todo_list,title: "New title",
-					 description: "New description"
-	todo_list.reload
-	
+	update_todo_list todo_list: todo_list, title: "New title", description: "New description"
+	todo_list.reload	
 	expect(page).to have_content("Todo list was sucessfully updated")
 	expect(todo_list.title).to eq("New title")
 	expect(todo_list.description).to eq("New Description")
+	end
+  it "displays an error with no title" do
+ 	update_todo_list todo_list: todo_list, title: ""
+	title = todo_list.title
+	todo_list.reload
+	expect(todo_list.title).to eq(title)
+	expect(page).to have_content("error")
+	end
+  it "displays an error with too short a title" do
+	update_todo_list todo_list: todo_list, title: "hi"
+	expect(page).to have_content("error")
+	end
+  it "displays an error with no description" do
+	update_todo_list todo_list: todo_list, description: ""
+	expect(page).to have_content("error")
+	end
+  it "displays an error with too short a description" do
+	update_todo_list todo_list: todo_list, description:"hi"
+	expect(page).to have_content("error")
+	end
   end
-end
 
